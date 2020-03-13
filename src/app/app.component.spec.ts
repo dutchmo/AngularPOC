368e554adcf98e14c42d03dc3acb6719c3ffbce3
Dutch <gmatous@rfdinc.com>
2020-03-13 14:45:58 -0500

    Initial checkin

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,78 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let debugElement: DebugElement;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    debugElement = fixture.debugElement;
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'AngularPOC'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('AngularPOC');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('AngularPOC app is running!');
   });
+
+  it('should increment/decrement value', () => {
+    fixture.componentInstance.increment();
+    expect(fixture.componentInstance.value).toEqual(1);
+
+    fixture.componentInstance.decrement();
+    expect(fixture.componentInstance.value).toEqual(0);
+  });
+
+  it('should increment in template', () => {
+    debugElement
+      .query(By.css('button.increment'))
+      .triggerEventHandler('click', null);
+
+    fixture.detectChanges();
+    const value = debugElement.query(By.css('h1')).nativeElement.innerText;
+    expect(value).toEqual('1');
+  });
+
+  it('should stop at 0 and show minimum message', () => {
+    debugElement
+      .query(By.css('button.decrement'))
+      .triggerEventHandler('click', null);
+
+    fixture.detectChanges();
+    const message = debugElement.query(By.css('p.message')).nativeElement.innerText;
+
+    expect(fixture.componentInstance.value).toEqual(0);
+    expect(message).toContain('Minimum');
+  });
+
+  it('should stop at 15 and show maximum message', () => {
+    fixture.componentInstance.value = 15;
+    debugElement
+      .query(By.css('button.increment'))
+      .triggerEventHandler('click', null);
+
+    fixture.detectChanges();
+    const message = debugElement.query(By.css('p.message')).nativeElement.innerText;
+
+    expect(fixture.componentInstance.value).toEqual(15);
+    expect(message).toContain('Maximum');
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Crudservice} from './services/Crudservice';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private crudService: Crudservice) {
+  }
+
+  ngOnInit(): void {
+    console.log(this.crudService.showTasks())
+  }
+
   title = 'AngularPOC';
+
+  value = 0;
+  message: string;
+
+  increment() {
+    console.log(this.crudService.showTasks())
+
+    if (this.value < 15) {
+      this.value += 1;
+      this.message = '';
+    } else {
+      this.message = 'Maximum reached!';
+    }
+  }
+  decrement() {
+    if (this.value > 0) {
+      this.value -= 1;
+      this.message = '';
+    } else {
+      this.message = 'Minimum reached!';
+    }
+  }
 }
